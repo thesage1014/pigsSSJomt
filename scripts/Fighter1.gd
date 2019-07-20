18a803519f96a0d38454e85e46c7c50a587fde48
Jonathan Minnamon <jminnamon@gmail.com>
2019-07-19 20:46:12 -0700

    basic input
    not connected to anything but the debug log yet

--- scripts/Fighter1.gd
+++ scripts/Fighter1.gd
@@ -3,6 +3,9 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var nextMove = 0
+var moveCount = 10
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,8 +13,21 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("ui_select"):
-		print("hooray")
-
+		print("Player 1 picked ", nextMove)
+		
+	elif event.is_action_released("ui_up"):
+		nextMove += 1
+		print(nextMove)
+	elif event.is_action_released("ui_down"):
+		nextMove -= 1
+		print(nextMove)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+func _process(delta):
+	if !nextMove:
+		nextMove = 0
+	elif nextMove > moveCount:
+		nextMove = 0
+	elif nextMove < 0:
+		nextMove = moveCount
 #	pass
